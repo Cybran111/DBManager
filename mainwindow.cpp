a6fbb3185006ad5c5f658933bdcd1c9bc01d03af
Nikita Lyubchich <cybran111@gmail.com>
2013-11-04 17:49:16 +0200

    Basic functions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QSqlDatabase"
+#include "QSqlQuery"
+#include "QDebug"
+#include "QtSql"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("enterprises");
+    db.setUserName("root");
+    db.setPassword("VT.YnT;uY@+lb83|yrSL]s<N!");
+    db.open();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::dbScriptExecute()
+
+{
+    QSqlQuery query;
+    query.exec(ui->scriptEdit->toPlainText());
+
+   ui->tableWidget->setColumnCount(query.record().count());
+   ui->tableWidget->setRowCount(query.size());
+
+    int index =0;
+    while (query.next()) {
+
+        for (int i=0; i < query.record().count(); i++)
+         {
+             ui->tableWidget->setItem(index,i, new QTableWidgetItem(query.value(i).toString()));
+         }
+        index++;
+     }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+
+
 private:
     Ui::MainWindow *ui;
+
+public slots:
+    void dbScriptExecute();
 };
 
 #endif // MAINWINDOW_H
